3ebcedccc5815559b12f0c1a76bf61788eadeed3
Ursula <Ursula@DESKTOP-A7M7K6F>
2021-07-25 19:36:15 +0200

    Agregar archivos de proyecto.

---
+++ WPFSample/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace WPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Values1 = new ChartValues<double> { 3, 4, 6, 3, 2, 6 };
+
+            DataContext = this;
+        }
+
+        public ChartValues<double> Values1 { get; set; }
+
+        void OnClick_blur(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This button does nothing..... ");
+
+        }
+
+    }
+
+
+}
